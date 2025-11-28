d94b2fb890814a7a35a4b89adbfe01f9baa6d81f
DrakXXX-boop <eh522322123@gmail.com>
2025-11-28 13:19:29 -0700

    Add files via upload

---
+++ proyecyo.html/script.js
@@ -0,0 +1,21 @@
+  // script js
+
+document.addEventListener('DOMContentLoaded',function())
+{
+  //boton y area de mensaje id
+const boton =
+document.getElementById('mi boton');
+const mensaje =
+document.getElementById('mensaje')
+
+  //escuchador de eventos al boton=ruido
+borron.addEventListener('click' function() {
+
+    //accion al hacer click
+    mensaje.textContent = 'El boton reacciono correctamente';
+               mensaje.style.color = 
+        'green';
+               botton.disabled = true;
+               //desactiva boton despues del click
+})
+}
