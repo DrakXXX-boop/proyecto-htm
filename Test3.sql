d307ee6a39cc2d6d8fcf376259b58c55e1a132a6
kalmatoypancito <felixtadeo532@gmail.com>
2025-11-28 13:37:58 -0700

    Add files via upload

---
+++ Test3.sql
@@ -0,0 +1,13 @@
+create database menoside;
+
+use menoside;
+
+create table menso(
+tonoto varchar(10),
+tarado varchar(20),
+soquete varchar(100)
+);
+insert into menso(tonoto, tarado,soquete)
+values ("ponte","a","chambear perro");
+
+select * from menso;
